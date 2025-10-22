e8278fba01ecf3e35ec4c889d37c64840e17d121
manvi59 <manviyogi59@gmail.com>
2025-10-22 18:05:48 +0530

    time complexity

---
+++ timecomplexity.cpp
@@ -0,0 +1,24 @@
+Big O notation-->upper bound 
+Theta notatin --> average case complexity
+omega notation --> Lower Bound complexity
+
+these all aare unit of mesurement 
+
+
+constant time --> O(1)-->0 to 10
+linear Time-->O(n)
+Logaritime time--> O(logn)-->Binaru Search 
+Quadratic time--> O(n^2)--> two loop
+cubic time-->O(n^3)-->three loop
+
+
+time complexity highest to lowest order ------->
+
+O(n!)
+O(2^n)
+O(n^3)
+O(n^2)
+O(n log n)
+O(n)
+O(logn)
+O(1)
