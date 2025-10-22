290a4dc00d006e5d9913af4bc26af1df0802dc8e
manvi59 <manviyogi59@gmail.com>
2025-10-22 10:39:55 +0530

    remove any duplicate char in the string update version

--- remove-duplicate-char.cpp
+++ remove-duplicate-char.cpp
@@ -12,6 +12,8 @@ int main(){
     
     // string str="programming";
     string str="banana";
+    string str="jjjjjjjxxxx&&44jwd";
+  
     int arr[26]={0};
 
     for(int i=0;i<str.length();i++){
@@ -41,3 +43,45 @@ int main(){
 
 
 // how to make the logic for the mix of char , symboles and and string
+
+//  string str="jjjjjjjxxxx&&44jwd"; --> for this kind of how to do it 
+
+// j=1
+// x=1
+// &=5
+// 4
+
+
+
+// this is for the removal for every char ----------------
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str="jjjjjjjxxxx&&44jwd";
+    string ans="";
+    
+    //ascii code 0 ,127
+    int arr[127]={0};
+    for(int i=0;i<str.length();i++){
+        int chr=str[i];
+        // cout<<chr<<endl;
+        arr[chr]=arr[chr]+1;
+       
+    }
+    
+    for(int i=0;i<str.length();i++){
+        int me=str[i];
+        
+        if(arr[me]>0){
+            
+            ans=ans+str[i];
+            arr[me]=0;
+        }
+        cout<<me<<endl;
+    }
+    
+     
+    cout<<endl;
+    cout<<ans;
+}
