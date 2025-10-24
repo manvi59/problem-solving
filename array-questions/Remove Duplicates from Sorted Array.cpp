703350da3b91a582d9af729ccd225aec0f6c140f
manvi59 <manviyogi59@gmail.com>
2025-10-24 17:40:01 +0530

    remove duplicate element from the sorted array

--- array-questions/Remove Duplicates from Sorted Array.cpp
+++ array-questions/Remove Duplicates from Sorted Array.cpp
@@ -1,6 +1,7 @@
 Remove Duplicates from Sorted Array
 
-LeetCode #26 (Google favorite)
+LeetCode #26 (Google favorite) 
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1810121764/
 
 Input: [1,1,2,2,3] → Output: [1,2,3]
 
@@ -58,8 +59,40 @@ int removeDuplicates(int* nums, int numsSize) {
 // nums =
 // [0,0,0,0,3]
 
+
 // Use Testcase
 // Output
 // [0,1,2,3]
 // Expected
 // [0,3]
+
+
+// ---------------------this is the correct logic to remove the duplicate array ..
+//---------------------this will work for every edge cases
+
+// #include<iostream>
+// using namespace std;
+int removeDuplicates(int* nums, int numsSize) {
+    
+     int ans[numsSize];
+    int count=0;
+
+    for(int i=0;i<numsSize-1;i++){
+
+        if(nums[i]!=nums[i+1] ){
+            ans[count]=nums[i];
+            count++;
+        }
+        
+    }
+    ans[count]=nums[numsSize-1];
+    // cout<<count<<endl;
+    
+
+    for(int i=0;i<=count;i++){
+        // cout<<ans[i]<<endl;
+        nums[i]=ans[i];
+    }
+    
+    return count+1;
+}
